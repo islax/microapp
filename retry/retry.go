6bcf328df512ef62ecf37ff4b51260a6c7788ea3
Ashish Bhat <ashish.bhat@aurionpro.com>
2019-06-29 15:30:50 +0530

    Added Retry utility package

---
+++ retry/retry.go
@@ -0,0 +1,28 @@
+package retry
+
+import "time"
+
+// Do Performs repeated calls with a time delay for specific number of attempts
+// or till the function returns no error
+// Return Stop{err}, if you want to stop despite Error
+func Do(attempts int, sleep time.Duration, fn func() error) error {
+	if err := fn(); err != nil {
+		if s, ok := err.(Stop); ok {
+			// Return the original error for later checking
+			return s.error
+		}
+
+		if attempts--; attempts > 0 {
+			time.Sleep(sleep)
+			return Do(attempts, 2*sleep, fn)
+		}
+		return err
+	}
+	return nil
+}
+
+// Stop is used to return error and stop retrying
+// Return Stop{err}, if you want to stop despite Error
+type Stop struct {
+	error
+}
