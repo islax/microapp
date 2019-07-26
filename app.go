51d4490cb07d1370e691e45e9301311fc850dcc4
Ashish Bhat <ashish.bhat@aurionpro.com>
2019-07-26 12:03:50 +0530

    Added migrations support

--- app.go
+++ app.go
@@ -2,10 +2,14 @@ package microapp
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
 
 	"time"
 
+	migrate "github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/gorilla/mux"
 	"github.com/islax/microapp/config"
 	"github.com/islax/microapp/event"
@@ -76,6 +80,43 @@ func (app *App) Logger(module string) *log.Entry {
 			"service": app.Name,
 			"module":  module,
 		})
+}
+
+// MigrateDB Looks for migrations directory and runs the migrations scripts in that directory
+func (app *App) MigrateDB(connectionString string) {
+	logger := app.log
+
+	logger.Info("============ DB Migration Begin ============")
+	fsrc, err := (&file.File{}).Open("file://migrations")
+	if err != nil {
+		logger.Info("No migrations directory found. Skipping migrations. Error: ", err)
+		logger.Info("============ DB Migration End ============")
+		return
+	}
+	migrateDB, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		logger.Fatal("Unable to open DB connection for migration: ", err)
+	}
+	migrateDBDriver, err := mysql.WithInstance(migrateDB, &mysql.Config{})
+	if err != nil {
+		logger.Error("Unable to prepare DB instance for migration: ", err)
+	}
+	m, err := migrate.NewWithInstance("file", fsrc, "mysql", migrateDBDriver)
+	if err != nil {
+		logger.Error("Unable to initialize DB instance for migration: ", err)
+	}
+	err = m.Up()
+	if err != nil {
+		if err.Error() == "no change" {
+			logger.Info("DB already in latest state.")
+		} else {
+			logger.Error("Failed to migrate DB: ", err)
+			// panic(err)
+		}
+	} else {
+		logger.Info("Successfully upgraded DB")
+	}
+	logger.Info("============ DB Migration End ============")
 }
 
 // Stop http server
