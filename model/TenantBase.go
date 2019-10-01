af0d45fbaf81619232f486787e3f060e3907db01
Mandar Ramdas <mandar.ramdas@gmail.com>
2019-10-01 17:31:49 +0530

    added indexes

--- model/TenantBase.go
+++ model/TenantBase.go
@@ -9,8 +9,8 @@ import (
 // TenantBase contains common columns for all tables that are tenant specific.
 type TenantBase struct {
 	ID        uuid.UUID  `gorm:"type:varchar(36);primary_key;"`
-	TenantID  uuid.UUID  `gorm:"type:varchar(36);column:tenantId;"`
-	CreatedAt time.Time  `gorm:"column:createdOn"`
+	TenantID  uuid.UUID  `gorm:"type:varchar(36);column:tenantId;index:tenantid"`
+	CreatedAt time.Time  `gorm:"column:createdOn;index:createdon"`
 	UpdatedAt time.Time  `gorm:"column:modifiedOn"`
 	DeletedAt *time.Time `sql:"index" gorm:"column:deletedOn"`
 }
