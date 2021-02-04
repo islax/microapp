a743f3a502f4ad2b700863ef8339e6795d6890d3
sreejith <sreejith.devadas@cyberinc.com>
2021-02-04 14:38:35 +0530

    new db in new unit of work

--- repository/Repository.go
+++ repository/Repository.go
@@ -46,9 +46,9 @@ type UnitOfWork struct {
 // NewUnitOfWork creates new UnitOfWork
 func NewUnitOfWork(db *gorm.DB, readOnly bool) *UnitOfWork {
 	if readOnly {
-		return &UnitOfWork{DB: db, committed: false, readOnly: true}
+		return &UnitOfWork{DB: db.Session(&gorm.Session{NewDB: true}), committed: false, readOnly: true}
 	}
-	return &UnitOfWork{DB: db.Begin(), committed: false, readOnly: false}
+	return &UnitOfWork{DB: db.Session(&gorm.Session{NewDB: true}).Begin(), committed: false, readOnly: false}
 }
 
 // Complete marks end of unit of work
