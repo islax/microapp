22d75c36fb62101934b3062771afd19db843b7a7
kn-cyberinc <kalaiselvan.nadar@cyberinc.com>
2021-06-02 14:02:34 -0700

    Updated aesDataCryptor

--- service/impl/aesDataCryptor.go
+++ service/impl/aesDataCryptor.go
@@ -5,11 +5,20 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	mathRand "math/rand"
+	"strings"
+	"time"
 
 	"github.com/islax/microapp"
 	microappError "github.com/islax/microapp/error"
 	"github.com/islax/microapp/service"
 	"golang.org/x/crypto/scrypt"
+)
+
+const (
+	extraSaltLength          = 12
+	randomPrefixStringLength = 8
+	version2Prefix           = "v2.0"
 )
 
 type aesDataCryptor struct {
@@ -22,9 +31,9 @@ func NewAESDataCryptor(app *microapp.App) service.DataCryptor {
 }
 
 func (cryptor *aesDataCryptor) Encrypt(data string, salt string) (string, error) {
+	salt, extraSaltAdded := cryptor.addSomeExtraSalt(salt)
 	key, err := cryptor.deriveKey(cryptor.app.Config.GetString("CRYPTO_KEY"), salt)
-	// key, err := cryptor.deriveKey("#some secret key#", salt)
-
+	// key, err := cryptor.deriveKey("some key###", salt)
 	blockCipher, err := aes.NewCipher(key)
 	if err != nil {
 		return "", microappError.NewUnexpectedError(microappError.ErrorCodeCryptoFailure, err)
@@ -41,14 +50,20 @@ func (cryptor *aesDataCryptor) Encrypt(data string, salt string) (string, error)
 	}
 
 	ciphertext := gcm.Seal(nonce, nonce, []byte(data), nil)
-	return base64.StdEncoding.EncodeToString(ciphertext), nil
+	return version2Prefix + cryptor.getRandomString(randomPrefixStringLength) + base64.StdEncoding.EncodeToString(ciphertext) + extraSaltAdded, nil
 }
 
 func (cryptor *aesDataCryptor) Decrypt(data string, salt string) (string, error) {
-	dataBytes, err := base64.StdEncoding.DecodeString(data)
+
+	extraSaltBeginIndex := len(data) - extraSaltLength
+	salt = salt + data[extraSaltBeginIndex:]
+	prefixLength := len(version2Prefix) + randomPrefixStringLength
+	base64EncodedDataString := data[prefixLength:extraSaltBeginIndex]
+	dataBytes, err := base64.StdEncoding.DecodeString(base64EncodedDataString)
 	if err != nil {
 		return "", microappError.NewUnexpectedError(microappError.ErrorCodeCryptoFailure, err)
 	}
+
 	key, err := cryptor.deriveKey(cryptor.app.Config.GetString("CRYPTO_KEY"), salt)
 	if err != nil {
 		return "", microappError.NewUnexpectedError(microappError.ErrorCodeCryptoFailure, err)
@@ -76,4 +91,21 @@ func (cryptor *aesDataCryptor) deriveKey(password, salt string) ([]byte, error) 
 		return nil, err
 	}
 	return key, nil
+}
+
+func (cryptor aesDataCryptor) addSomeExtraSalt(salt string) (string, string) {
+	extraSalt := cryptor.getRandomString(extraSaltLength)
+	return salt + extraSalt, extraSalt
+}
+
+func (cryptor aesDataCryptor) getRandomString(length int) string {
+	//generate random string
+	mathRand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&[]{}*()-=_+|;',./:<>?")
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[mathRand.Intn(len(chars))])
+	}
+
+	return b.String()
 }
