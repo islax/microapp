a504e3419cf576870c6e885e40fe7a80b64cb9e4
Dhaval Gogri <dhaval.gogri@ckomac-6.local>
2021-09-22 17:08:45 -0700

    Changed PromptDetails to PromptDetail

--- settingsmetadata/model/PromptDetail.go
+++ settingsmetadata/model/PromptDetail.go
@@ -1,7 +1,7 @@
 package model
 
-// PromptDetails contains the metadata regarding prompt for data change
-type PromptDetails struct {
+// PromptDetail contains the metadata regarding prompt for data change
+type PromptDetail struct {
 	PromptTitle          string        `json:"promptTitle"`
 	PromptMessage        string        `json:"promptMessage"`
 	ShowPromptWhenValues []interface{} `json:"showPromptWhenValues"` // If blank, shows prompt on every value change
