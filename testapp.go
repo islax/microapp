b15934accd0eb9114fbc463ea0768362d1b388da
Rachna Jaju <rachnajaju@wsloa01s-MacBook-Pro.local>
2022-03-31 12:00:11 +0530

    added test

--- testapp.go
+++ testapp.go
@@ -178,6 +178,13 @@ func (testApp *TestApp) Query(name string, args []interface{}) (*sql.Rows, error
 // Initialize prepares the app for testing
 func (testApp *TestApp) Initialize() {
 	testApp.application.Initialize(testApp.controllerRouteProvider(testApp.application))
+	testApp.PrepareEmptyTables()
+	go testApp.application.Start()
+}
+
+// Init prepares the app for testing
+func (testApp *TestApp) Init() {
+	testApp.application.Initialize(testApp.controllerRouteProvider(testApp.application))
 	//testApp.PrepareEmptyTables()
 	go testApp.application.Start()
 }
