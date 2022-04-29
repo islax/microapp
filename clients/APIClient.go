7938c71023c7831f46da0bd0b192b6f5045d49bc
Anirudh <anirudh.arora@cyberinc.com>
2022-04-28 21:25:40 -0700

    push origin

--- clients/APIClient.go
+++ clients/APIClient.go
@@ -36,7 +36,7 @@ func (apiClient *APIClient) getJSONRequestBody(payload interface{}) (io.Reader, 
 
 // DoRequestBasic ...
 func (apiClient *APIClient) DoRequestBasic(context microappCtx.ExecutionContext, uri string, requestMethod string, rawToken string, payload interface{}) (*http.Response, microappError.APIClientError) {
-	apiURL := apiClient.BaseURL + url.QueryEscape(uri)uri
+	apiURL := apiClient.BaseURL + url.QueryEscape(uri)
 
 	payloadAsIOReader, err := apiClient.getJSONRequestBody(payload)
 	if err != nil {
